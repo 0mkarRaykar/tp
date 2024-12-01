c2064f637fc54c005cfbf2eccfdf6e1ddb185a94
Tanmay Kadam <justcoding1482@gmail.com>
2024-12-01 20:38:39 +0530

    Initial commit for new repository

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,10 @@
-const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-  };
+const asyncHandler = (fn) => async (event, context) => {
+  try {
+      return await fn(event, context);  // Directly return the result from the async function
+  } catch (err) {
+      return {
+          statusCode: 500,
+          body: JSON.stringify({ error: 'Internal Server Error', message: err.message }),
+      };
+  }
 };
-
-export { asyncHandler };
